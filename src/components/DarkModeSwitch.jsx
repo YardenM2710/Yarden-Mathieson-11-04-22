188e4837279e8f5f5da7d8dd16b2b2260a37ce4b
Yarden Mathieson <yardenm2710@gmail.com>
2022-10-26 12:10:53 +0300

    added https

--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -1,35 +1,25 @@
-import React, { useEffect, useRef, useState } from 'react';
-import LightModeIcon from '@mui/icons-material/LightMode';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
+import React, { useEffect, useRef, useState } from 'react'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
 
 export default function DarkModeSwitch({ toggleDarkMode }) {
-  const checkBox = useRef(null);
-  const label = useRef(null);
-  const [isDark, setIsDark] = useState(false);
+  const checkBox = useRef(null)
+  const label = useRef(null)
+  const [isDark, setIsDark] = useState(false)
   function handleDarkMode(val) {
-    setIsDark(val);
-    toggleDarkMode(val);
+    setIsDark(val)
+    toggleDarkMode(val)
   }
 
   return (
     <div className="dark-mode-switch">
-      <input
-        onChange={(ev) => handleDarkMode(ev.target.checked)}
-        type="checkbox"
-        className="checkbox"
-        ref={checkBox}
-        id="checkbox"
-      />
+      <input onChange={(ev) => handleDarkMode(ev.target.checked)} type="checkbox" className="checkbox" ref={checkBox} id="checkbox" />
 
-      <label
-        ref={label}
-        htmlFor="checkbox"
-        className={isDark ? 'label dark' : 'label'}
-      >
+      <label ref={label} htmlFor="checkbox" className={isDark ? 'label dark' : 'label'}>
         <DarkModeIcon className="fas fa-moon" />
         <LightModeIcon className="fas fa-sun" />
         <div className="ball"></div>
       </label>
     </div>
-  );
+  )
 }
--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -1,27 +1,25 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
-import MenuIcon from '@mui/icons-material/Menu';
-import DarkModeSwitch from './DarkModeSwitch';
+import React, { useEffect, useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
+import MenuIcon from '@mui/icons-material/Menu'
+import DarkModeSwitch from './DarkModeSwitch'
 
 export function MainHeader({ toggleDarkMode }) {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const navbar = useRef(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const navbar = useRef(null)
 
   const toggleMenu = () => {
-    setIsMenuOpen((isMenuOpen) => (isMenuOpen = !isMenuOpen));
-  };
+    setIsMenuOpen((isMenuOpen) => (isMenuOpen = !isMenuOpen))
+  }
 
   useEffect(() => {
-    isMenuOpen
-      ? (navbar.current.style.transform = 'translate(0)')
-      : (navbar.current.style.transform = 'translate(100%)');
-  }, [isMenuOpen]);
+    isMenuOpen ? (navbar.current.style.transform = 'translate(0)') : (navbar.current.style.transform = 'translate(100%)')
+  }, [isMenuOpen])
 
   return (
     <header className="app-header">
       {isMenuOpen && <div onClick={toggleMenu} className="screen"></div>}
       <section>
-        <h1 className="logo">Abra Weather</h1>
+        <h1 className="logo">MyWeather</h1>
         <DarkModeSwitch toggleDarkMode={toggleDarkMode} />
 
         <div onClick={toggleMenu} className="menu">
@@ -37,5 +35,5 @@ export function MainHeader({ toggleDarkMode }) {
         </nav>
       </section>
     </header>
-  );
+  )
 }
